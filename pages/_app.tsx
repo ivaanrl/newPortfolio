e195c90845e45f1096ee4b35231deb2105d783d9
ivaanrl <ivanrl2010@gmail.com>
2020-12-13 20:06:01 -0300

    add background animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,44 @@ import Footer from '../containers/footer/footer';
 import Navbar from '../containers/navbar/navbar';
 import ContactForm from '../containers/contactForm/contactForm';
 import '../styles/globals.scss';
+import Background from '../containers/background/background';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   const [showContactForm, setShowContactForm] = useState<boolean>(false);
+  const x = useMotionValue(0.5);
+  const y = useMotionValue(0.5);
+
+  const onMove = (e) => {
+    const bounds = e.currentTarget.getBoundingClientRect();
+
+    // set x,y local coordinates
+    const xValue = (e.clientX - bounds.x) / e.currentTarget.clientWidth;
+    const yValue = (e.clientY - bounds.y) / e.currentTarget.clientHeight;
+
+    // update MotionValues
+    x.set(xValue, true);
+    y.set(yValue, true);
+  };
+
+  const translateX = useTransform(x, [0, 1], [-2, 2], {
+    clamp: true,
+  });
+
+  const translateY = useTransform(y, [0, 1], [-5, 5], {
+    clamp: true,
+  });
 
   return (
-    <>
+    <motion.div onPointerMove={onMove}>
+      <Background translateX={translateX} translateY={translateY} />
       <Navbar showContactForm={() => setShowContactForm(true)} />
       <Component {...pageProps} />
       <Footer showContactForm={() => setShowContactForm(true)} />
       {showContactForm ? (
         <ContactForm hideContactForm={() => setShowContactForm(false)} />
       ) : null}
-    </>
+    </motion.div>
   );
 }
 

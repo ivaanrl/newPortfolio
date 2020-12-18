7c5d74d5cd234118125e9b9338aa223a3a5efe59
ivaanrl <ivanrl2010@gmail.com>
2020-12-18 19:14:04 -0300

    add next-pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
             rel="stylesheet"
           />
+
           <link rel="shortcut icon" href="icon/favicon.png" />
+          <link rel="apple-touch-icon" href="icon/appleTouchIcon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <meta name="theme-color" content="#000000" />
         </Head>
         <body>
           <Main />
